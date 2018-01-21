1e9017f8b341c68f73eceb99f15f55538ebb5925
Paweł Galeja <pgaleja@gmail.com>
2018-01-21 21:53:55 +0100

    Change errors in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,41 @@
 var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
-var wuoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    $.getJSON(prefix + quoteUrl, createTweet);
-    $.ajaxSetup({ cache: false });
+	$.getJSON(prefix + quoteUrl, createTweet);
+	$.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
-    var data = input[0];
-    
-    var quoteText = $(data.content).text().trim();
-    var quoteAuthor = data.title;
+	var data = input[0];
 
-    if(!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
-    }
+	if (!input.length) {
+    	return;
+	}
 
-    var tweetText = "Quote of the day - " + quoteText + "Author: " + quoteAuthor;
+	var quoteText = $(data.content).text().trim();
+	var quoteAuthor = data.title;
 
-    if (tweetText.length > 140) {
-        getQuote();
-    } else {
-        var tweet = tweetLink + encodeURIComponent(tweetText);
-        $('.quote').text(quoteText);
-        $('.author').text("Author: " + quoteAuthor);
-        $('.tweet').attr('href', tweet);
-    }
+	if (!quoteAuthor.length) {
+		quoteAuthor = "Unknown author";
+	}
+
+	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+	if (tweetText.length > 280) {
+		getQuote();
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+		$('.quote').text(quoteText);
+		$('.author').text("Author: " + quoteAuthor);
+		$('.tweet').attr('href', tweet);
+	}
 }
 
 $(document).ready(function() {
-    getQuote();
-    $('.trigger').on('click', function(){
-        getQuote();
-    });
+	getQuote();
+	$('.trigger').on('click', function() {
+		getQuote();
+	});
 });
